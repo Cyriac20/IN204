f5ceec7e95a0c39fa744cb2cfd898be21fecd6b9
Cyriac <cyriac.salignat@ensta.fr>
2025-12-15 09:22:13 +0100

    update 15 décembre

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
 
@@ -6,6 +7,22 @@ int main()
 {
     // SFML 3: VideoMode takes a Vector2u for size
     RenderWindow window(VideoMode({800u, 600u}), "Mon Titre");
+    window.setVerticalSyncEnabled(true);
+    Font police;
+    
+
+    if (!police.openFromFile("../src/res/poppins.ttf")) {
+        std::cerr << "Erreur lors du chargement de la police!" << std::endl;
+        return -1;
+    }
+
+    String texteAccentue = "Bonjour avec ma police personnalisée!";
+    Text texte(police);
+    texte.setString(texteAccentue);
+    texte.setCharacterSize(30); // Taille en pixels
+    texte.setFillColor(Color::White);
+    texte.setStyle(Text::Bold); // Optionnel: Bold, Italic, Underlined
+    texte.setPosition({100, 250});
 
     while (window.isOpen())
     {
@@ -17,10 +34,11 @@ int main()
             {
                 window.close();
             }
+
         }
 
-        window.clear();
-        // Your drawing code here
+        window.clear(Color::Black);
+        window.draw(texte);
         window.display();
     }
 
