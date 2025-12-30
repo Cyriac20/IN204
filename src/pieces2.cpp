f2a2e7b3e67bb47d4c84ea45499ceff7c21da835
Theotime110 <theotime.galmiche@ensta.fr>
2025-12-30 18:25:29 +0100

    pieces.cpp et pieces.hpp

---
+++ src/pieces2.cpp
@@ -0,0 +1,130 @@
+#include "pieces.hpp"
+
+Piece::Piece() {};
+
+//----------------------- Piece O ---------------------------
+
+PieceO::PieceO(){
+    position = {{
+                {0,4}, {0,5}, 
+                {1,4}, {1,5}
+            }};
+}
+
+void PieceO::rotate() {}; //La rotation n'a pas d'effet
+
+
+//----------------------- Piece I ----------------------------
+
+PieceI::PieceI(){
+    position = {{
+                {0,3}, {0,4}, {0,5}, {0,6}
+            }};
+}
+
+void PieceI::rotate() {
+    //On doit définir un pivot de rotation (ici c'est le deuxième de la ligne)
+
+    int pivotX = position[1][1]; 
+    int pivotY = position[1][0]; 
+
+    for (int i = 0; i < 4; ++i) {
+        int x = position[i][1] - pivotX; // colonne relative
+        int y = position[i][0] - pivotY; // ligne relative
+
+        // On effectue la rotation de 90° autour du pivot
+        position[i][1] = pivotX - y;
+        position[i][0] = pivotY + x;
+
+}
+
+
+//----------------------- Piece T ---------------------------
+PieceT::PieceT(){
+    position = {{ {0,4}, {1,3}, {1,4}, 
+                        {1,5} }};
+}
+
+void PieceT::rotate() {
+    int pivotX = position[1][1];
+    int pivotY = position[1][0];
+    for (int i = 0; i < 4; ++i) {
+        int x = position[i][1] - pivotX;
+        int y = position[i][0] - pivotY;
+
+        position[i][1] = pivotX - y;
+        position[i][0] = pivotY + x;
+    }
+}
+
+//----------------------- Piece L ---------------------------
+PieceL::PieceL(){
+    position = {{ {0,4}, {1,4}, {2,4}, 
+                {2,5} }};
+}
+
+void PieceL::rotate() {
+    int pivotX = position[1][1];
+    int pivotY = position[1][0];
+    for (int i = 0; i < 4; ++i) {
+        int x = position[i][1] - pivotX;
+        int y = position[i][0] - pivotY;
+
+        position[i][1] = pivotX - y;
+        position[i][0] = pivotY + x;
+    }
+}
+
+//----------------------- Piece J ---------------------------
+PieceJ::PieceJ(){
+    position = {{ {0,4}, {1,4}, {2,4}, 
+                                {2,3} }};
+}
+
+void PieceJ::rotate() {
+    int pivotX = position[1][1];
+    int pivotY = position[1][0];
+    for (int i = 0; i < 4; ++i) {
+        int x = position[i][1] - pivotX;
+        int y = position[i][0] - pivotY;
+
+        position[i][1] = pivotX - y;
+        position[i][0] = pivotY + x;
+    }
+}
+
+//----------------------- Piece S ---------------------------
+PieceS::PieceS(){
+    position = {{ {0,4}, {0,5}, 
+        {1,3}, {1,4} }};
+}
+
+void PieceS::rotate() {
+    int pivotX = position[1][1];
+    int pivotY = position[1][0];
+    for (int i = 0; i < 4; ++i) {
+        int x = position[i][1] - pivotX;
+        int y = position[i][0] - pivotY;
+
+        position[i][1] = pivotX - y;
+        position[i][0] = pivotY + x;
+    }
+}
+
+//----------------------- Piece Z ---------------------------
+PieceZ::PieceZ(){
+    position = {{ {0,4}, {0,5}, 
+                        {1,5}, {1,6} }};
+}
+
+void PieceZ::rotate() {
+    int pivotX = position[1][1];
+    int pivotY = position[1][0];
+    for (int i = 0; i < 4; ++i) {
+        int x = position[i][1] - pivotX;
+        int y = position[i][0] - pivotY;
+
+        position[i][1] = pivotX - y;
+        position[i][0] = pivotY + x;
+    }
+}
---
+++ src/pieces2.hpp
@@ -0,0 +1,71 @@
+#ifndef PIECES_HPP
+#define PIECES_HPP
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <array>
+
+class Piece {
+    protected:
+        Piece(); //On ne crée pas directement la pièce dans la classe mère
+    
+    public:
+        std::array<std::array<int,2>,4> position;
+    
+    
+        virtual ~Piece() = default; //évite les fuites mémoires
+        
+        virtual void rotate() = 0; //indique que la rotation des pièces est propre à chaque pièce
+
+};
+
+
+// Pièce I
+class PieceI : public Piece {
+public:
+    PieceI();
+    void rotate() override; // rotation spécifique
+};
+
+// Pièce O
+class PieceO : public Piece {
+public:
+    PieceO();
+    void rotate() override; // rotation spécifique (pas de rotation)
+};
+
+// Pièce T
+class PieceT : public Piece {
+public:
+    PieceT();
+    void rotate() override; // rotation spécifique
+};
+
+// Pièce L
+class PieceL : public Piece {
+public:
+    PieceL();
+    void rotate() override; // rotation spécifique
+};
+
+// Pièce J
+class PieceJ : public Piece {
+public:
+    PieceI();
+    void rotate() override; // rotation spécifique
+};
+
+// Pièce Z
+class PieceZ : public Piece {
+public:
+    PieceZ();
+    void rotate() override; // rotation spécifique
+};
+
+// Pièce S
+class PieceS : public Piece {
+public:
+    PieceS();
+    void rotate() override; // rotation spécifique
+};
+#endif
